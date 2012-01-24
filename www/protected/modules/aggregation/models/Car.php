33495c358bd41f48575823944a0fd262fbd15f35
Boris Serebrov <seb@appto.us>
2012-01-24 13:09:40 +0200

    fix instantiate method

--- www/protected/modules/aggregation/models/Car.php
+++ www/protected/modules/aggregation/models/Car.php
@@ -1,42 +1,33 @@
 <?php
 
 /**
- * This is the model class for table "car".
- *
- * The followings are the available columns in table 'car':
- * @property integer $id
- * @property string $name
- * @property string $type
- */
+* This is the model class for table "car".
+*
+* The followings are the available columns in table 'car':
+* @property integer $id
+* @property string $name
+* @property string $type
+*/
 class Car extends CActiveRecord
 {
-	/**
-	 * Returns the static model of the specified AR class.
-	 * @return Car the static model class
-	 */
-	public static function model($className=__CLASS__)
-	{
-		return parent::model($className);
-	}
+    /**
+    * Returns the static model of the specified AR class.
+    * @return Car the static model class
+    */
+    public static function model($className=__CLASS__)
+    {
+        return parent::model($className);
+    }
 
-   /**
-     * We're overriding this method to fill findAll() and similar method result
-     * with proper models.
-     *
-     * @param array $attributes
-     * @return Car
-     */
+/**
+    * We're overriding this method to fill findAll() and similar method result
+    * with proper models.
+    *
+    * @param array $attributes
+    * @return Car
+    */
     protected function instantiate($attributes){
-        switch($attributes['type']){
-            case 'sport':
-                $class='SportCar';
-            break;
-            case 'family':
-                $class='FamilyCar';
-            break;
-            default:
-                $class=get_class($this);
-        }
+        $class = $attributes['type'];
         $model=new $class(null);
         return $model;
     }
@@ -48,72 +39,72 @@ class Car extends CActiveRecord
         return parent::beforeSave();
     }
 
-	/**
-	 * @return string the associated database table name
-	 */
-	public function tableName()
-	{
-		return 'car';
-	}
+    /**
+    * @return string the associated database table name
+    */
+    public function tableName()
+    {
+        return 'car';
+    }
 
-	/**
-	 * @return array validation rules for model attributes.
-	 */
-	public function rules()
-	{
-		// NOTE: you should only define rules for those attributes that
-		// will receive user inputs.
-		return array(
-			array('name', 'length', 'max'=>45),
-			array('type', 'length', 'max'=>9),
-			// The following rule is used by search().
-			// Please remove those attributes that should not be searched.
-			array('id, name, type', 'safe', 'on'=>'search'),
-		);
-	}
+    /**
+    * @return array validation rules for model attributes.
+    */
+    public function rules()
+    {
+        // NOTE: you should only define rules for those attributes that
+        // will receive user inputs.
+        return array(
+            array('name', 'length', 'max'=>45),
+            array('type', 'length', 'max'=>9),
+            // The following rule is used by search().
+            // Please remove those attributes that should not be searched.
+            array('id, name, type', 'safe', 'on'=>'search'),
+        );
+    }
 
-	/**
-	 * @return array relational rules.
-	 */
-	public function relations()
-	{
-		// NOTE: you may need to adjust the relation name and the related
-		// class name for the relations automatically generated below.
-		return array(
-		);
-	}
+    /**
+    * @return array relational rules.
+    */
+    public function relations()
+    {
+        // NOTE: you may need to adjust the relation name and the related
+        // class name for the relations automatically generated below.
+        return array(
+        );
+    }
 
-	/**
-	 * @return array customized attribute labels (name=>label)
-	 */
-	public function attributeLabels()
-	{
-		return array(
-			'id' => 'Id',
-			'name' => 'Name',
-			'type' => 'Type',
-		);
-	}
+    /**
+    * @return array customized attribute labels (name=>label)
+    */
+    public function attributeLabels()
+    {
+        return array(
+            'id' => 'Id',
+            'name' => 'Name',
+            'type' => 'Type',
+        );
+    }
 
-	/**
-	 * Retrieves a list of models based on the current search/filter conditions.
-	 * @return CActiveDataProvider the data provider that can return the models based on the search/filter conditions.
-	 */
-	public function search()
-	{
-		// Warning: Please modify the following code to remove attributes that
-		// should not be searched.
+    /**
+    * Retrieves a list of models based on the current search/filter conditions.
+    * @return CActiveDataProvider the data provider that can return the models based on the search/filter conditions.
+    */
+    public function search()
+    {
+        // Warning: Please modify the following code to remove attributes that
+        // should not be searched.
 
-		$criteria=new CDbCriteria;
+        $criteria=new CDbCriteria;
 
-		$criteria->compare('id',$this->id);
+        $criteria->compare('id',$this->id);
 
-		$criteria->compare('name',$this->name,true);
+        $criteria->compare('name',$this->name,true);
 
-		$criteria->compare('type',$this->type,true);
+        $criteria->compare('type',$this->type,true);
 
-		return new CActiveDataProvider('Car', array(
-			'criteria'=>$criteria,
-		));
-	}
+        return new CActiveDataProvider('Car', array(
+            'criteria'=>$criteria,
+        ));
+    }
 }
--- www/protected/modules/aggregation/models/FamilyCarData.php
+++ www/protected/modules/aggregation/models/FamilyCarData.php
@@ -1,87 +1,87 @@
 <?php
 
 /**
- * This is the model class for table "family_car_data".
- *
- * The followings are the available columns in table 'family_car_data':
- * @property integer $car_id
- * @property integer $seats
- */
+* This is the model class for table "family_car_data".
+*
+* The followings are the available columns in table 'family_car_data':
+* @property integer $car_id
+* @property integer $seats
+*/
 class FamilyCarData extends CActiveRecord
 {
-	/**
-	 * Returns the static model of the specified AR class.
-	 * @return FamilyCarData the static model class
-	 */
-	public static function model($className=__CLASS__)
-	{
-		return parent::model($className);
-	}
+    /**
+    * Returns the static model of the specified AR class.
+    * @return FamilyCarData the static model class
+    */
+    public static function model($className=__CLASS__)
+    {
+        return parent::model($className);
+    }
 
-	/**
-	 * @return string the associated database table name
-	 */
-	public function tableName()
-	{
-		return 'family_car_data';
-	}
+    /**
+    * @return string the associated database table name
+    */
+    public function tableName()
+    {
+        return 'family_car_data';
+    }
 
-	/**
-	 * @return array validation rules for model attributes.
-	 */
-	public function rules()
-	{
-		// NOTE: you should only define rules for those attributes that
-		// will receive user inputs.
-		return array(
-			array('car_id, seats', 'required'),
-			array('car_id, seats', 'numerical', 'integerOnly'=>true),
-			// The following rule is used by search().
-			// Please remove those attributes that should not be searched.
-			array('car_id, seats', 'safe', 'on'=>'search'),
-		);
-	}
+    /**
+    * @return array validation rules for model attributes.
+    */
+    public function rules()
+    {
+        // NOTE: you should only define rules for those attributes that
+        // will receive user inputs.
+        return array(
+            array('car_id, seats', 'required'),
+            array('car_id, seats', 'numerical', 'integerOnly'=>true),
+            // The following rule is used by search().
+            // Please remove those attributes that should not be searched.
+            array('car_id, seats', 'safe', 'on'=>'search'),
+        );
+    }
 
-	/**
-	 * @return array relational rules.
-	 */
-	public function relations()
-	{
-		// NOTE: you may need to adjust the relation name and the related
-		// class name for the relations automatically generated below.
-		return array(
-			'car' => array(self::BELONGS_TO, 'Car', 'car_id'),
-		);
-	}
+    /**
+    * @return array relational rules.
+    */
+    public function relations()
+    {
+        // NOTE: you may need to adjust the relation name and the related
+        // class name for the relations automatically generated below.
+        return array(
+            'car' => array(self::BELONGS_TO, 'Car', 'car_id'),
+        );
+    }
 
-	/**
-	 * @return array customized attribute labels (name=>label)
-	 */
-	public function attributeLabels()
-	{
-		return array(
-			'car_id' => 'Car',
-			'seats' => 'Seats',
-		);
-	}
+    /**
+    * @return array customized attribute labels (name=>label)
+    */
+    public function attributeLabels()
+    {
+        return array(
+            'car_id' => 'Car',
+            'seats' => 'Seats',
+        );
+    }
 
-	/**
-	 * Retrieves a list of models based on the current search/filter conditions.
-	 * @return CActiveDataProvider the data provider that can return the models based on the search/filter conditions.
-	 */
-	public function search()
-	{
-		// Warning: Please modify the following code to remove attributes that
-		// should not be searched.
+    /**
+    * Retrieves a list of models based on the current search/filter conditions.
+    * @return CActiveDataProvider the data provider that can return the models based on the search/filter conditions.
+    */
+    public function search()
+    {
+        // Warning: Please modify the following code to remove attributes that
+        // should not be searched.
 
-		$criteria=new CDbCriteria;
+        $criteria=new CDbCriteria;
 
-		$criteria->compare('car_id',$this->car_id);
+        $criteria->compare('car_id',$this->car_id);
 
-		$criteria->compare('seats',$this->seats);
+        $criteria->compare('seats',$this->seats);
 
-		return new CActiveDataProvider('FamilyCarData', array(
-			'criteria'=>$criteria,
-		));
-	}
+        return new CActiveDataProvider('FamilyCarData', array(
+            'criteria'=>$criteria,
+        ));
+    }
 }
